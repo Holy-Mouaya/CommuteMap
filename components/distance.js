4ef2b79f425b2e012dea56083a32328640e28885
Holy Mouaya <holymaels.mouaya@mail.mcgill.ca>
2022-12-18 18:08:31 -0500

    Initial commit, map added

---
+++ components/distance.js
@@ -0,0 +1,12 @@
+const commutesPerYear = 260 * 2;
+const litresPerKM = 10 / 100;
+const gasLitreCost = 1.5;
+const litreCostKM = litresPerKM * gasLitreCost;
+const secondsPerDay = 60 * 60 * 24;
+
+
+function Distance({ leg }) {
+  return <div>Distance</div>;
+}
+
+export default Distance;
